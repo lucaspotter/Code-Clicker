bb7c2aa307a34ef150ed80139072e036ec89425c
Lucas Potter <pottel157@student.capsk12.org>
2021-02-08 14:31:26 +0000

    made price go up each time items are bought, updated readme

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ## clicky clicky
 
+Code Clicker is a ~~not so~~ professional clicker game made ~~by me in my room~~ by a professional game studio.
 
+## click until you drop
+
+With 7 upgrades, such as radiation caused extra hand growth and a Stack Overflow crutch, you'll be sure to have ~~maybe an hour~~ days of entertainment.
+
+## joint snappingly good
+
+You'll be sure to have fun with Code Clicker, a Lucas Potter production.
--- script.js
+++ script.js
@@ -5,16 +5,23 @@ var upgrade4 = 0;
 var upgrade5 = 0;
 var upgrade6 = 0;
 var upgrade7 = 0;
-var upgrade1Price = 15;
+var upgrade1Price = 0;
 var upgrade2Price = 100;
 var upgrade3Price = 500;
 var upgrade4Price = 3000;
 var upgrade5Price = 25000;
 var upgrade6Price = 100000;
 var upgrade7Price = 1000000;
+var site1Price = upgrade1Price + " Lines of Code"
+var site2Price = upgrade2Price + " Lines of Code"
+var site3Price = upgrade3Price + " Lines of Code"
+var site4Price = upgrade4Price + " Lines of Code"
+var site5Price = upgrade5Price + " Lines of Code"
+var site6Price = upgrade6Price + " Lines of Code"
+var site7Price = upgrade7Price + " Lines of Code"
 var linesOfCode = 0;
 var CPS = 0;
-var CPC = 1;
+var CPC = 0;
 
 // you have to make your own sleep function
 function sleep(milliseconds) {
@@ -27,58 +34,114 @@ function sleep(milliseconds) {
 
 
 function buyUpgrade1() {
-	linesOfCode -= 15
+	if (linesOfCode > upgrade1Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade1Price
 	CPC += 1
 	upgrade1 += 1
+	upgrade1Price += 5
+	site1Price = upgrade1Price + " Lines of Code"
 	document.getElementById("upgrade1").innerHTML = upgrade1;
+	document.getElementById("upgrade1Price").innerHTML = site1Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
 function buyUpgrade2() {
-	linesOfCode -= 100
+	if (linesOfCode < upgrade2Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade2Price
 	CPS += 1
 	upgrade2 += 1
+	upgrade2Price += 20
+	site2Price = upgrade2Price + " Lines of Code"
 	document.getElementById("upgrade2").innerHTML = upgrade2;
+	document.getElementById("upgrade2Price").innerHTML = site2Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
 function buyUpgrade3() {
-	linesOfCode -= 500
+	if (linesOfCode < upgrade3Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade3Price
 	CPS += 3
 	upgrade3 += 1
+	upgrade3Price += 100
+	site3Price = upgrade3Price + " Lines of Code"
 	document.getElementById("upgrade3").innerHTML = upgrade3;
+	document.getElementById("upgrade3Price").innerHTML = site3Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
 function buyUpgrade4() {
-	linesOfCode -= 3000
+	if (linesOfCode < upgrade4Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade4Price
 	CPS += 5
 	upgrade4 +=1
+	upgrade4Price += 500
+	site4Price = upgrade4Price + " Lines of Code"
 	document.getElementById("upgrade4").innerHTML = upgrade4;
+	document.getElementById("upgrade4Price").innerHTML = site4Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
 function buyUpgrade5() {
-	linesOfCode -= 25000
+	if (linesOfCode < upgrade5Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade5Price
 	CPS += 20
 	upgrade5 +=1
+	upgrade5Price += 2500
+	site5Price = upgrade5Price + " Lines of Code"
 	document.getElementById("upgrade5").innerHTML = upgrade5;
+	document.getElementById("upgrade5Price").innerHTML = site5Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
 function buyUpgrade6() {
-	linesOfCode -= 100000
+	if (linesOfCode < upgrade6Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade6Price
 	CPS += 50
 	upgrade6 +=1
+	upgrade6Price += 10000
+	site6Price = upgrade6Price + "Lines of Code"
 	document.getElementById("upgrade6").innerHTML = upgrade6;
+	document.getElementById("upgrade6Price").innerHTML = site6Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
 function buyUpgrade7() {
-	linesOfCode -= 1000000
+	if (linesOfCode < upgrade7Price) {
+		alert("You do not have enough lines of code!")
+		return;
+	}
+
+	linesOfCode -= upgrade7Price
 	CPS += 100
 	upgrade7 +=1
+	upgrade7Price += 500
+	site7Price = upgrade7Price + " Lines of Code"
 	document.getElementById("upgrade7").innerHTML = upgrade7;
+	document.getElementById("upgrade7Price").innerHTML = site7Price;
 	document.getElementById("count").innerHTML = linesOfCode;
 }
 
